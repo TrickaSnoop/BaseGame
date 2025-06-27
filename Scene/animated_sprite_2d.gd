c298390ea17b3847ff1227aba6063c7d53f4227d
Bulbaloda <streetspoof@gmail.com>
2025-06-27 22:12:11 +0530

    sixth commit

--- Scene/animated_sprite_2d.gd
+++ Scene/animated_sprite_2d.gd
@@ -3,15 +3,26 @@ const SPEED = 40
 var dir = 1
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var ray_cast_2d_2: RayCast2D = $RayCast2D2
+@onready var ray_cast_2d_right: RayCast2D = $RayCast2DRIGHTTILT
+@onready var ray_cast_2d_2_left: RayCast2D = $RayCast2DLEFTTILT
 @onready var ass: AnimatedSprite2D = $"."
 
-func _process(delta):
+func _physics_process(delta):
 	if ray_cast_2d.is_colliding():
 		dir = -1
 		ass.flip_h = true
 	
-	if ray_cast_2d_2.is_colliding():
+	elif ray_cast_2d_2.is_colliding():
 		dir = 1 
 		ass.flip_h = false
+		
+	elif not ray_cast_2d_right.is_colliding():
+		dir *= -1
+		ass.flip_h = true
+	
+	elif not ray_cast_2d_2_left.is_colliding():
+		dir *= -1
+		ass.flip_h = false
+		
 	position.x += SPEED * delta * dir
 	
